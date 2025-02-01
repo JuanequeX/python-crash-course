e464e8e7f826ccb4741492b072b2bcbc47bfbf69
Juan Aguilar <50384228+JuanequeX@users.noreply.github.com>
2025-01-31 23:27:35 -0600

    [chapter 4] numerical list

--- chapter_four/numerical_lists.py
+++ chapter_four/numerical_lists.py
@@ -30,7 +30,38 @@ print(cubed)
 
 
 # Simple Statistics with a List of Numbers
+# with this example i can easily calculate the minimum, maximum, and sum of a list of numbers
 digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(min(digits))
 print(max(digits))
 print(sum(digits))
+
+# List Comprehensions
+# A list comprehension allows you to generate this same list in just one line of code
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
+
+twenties = [value for value in range(1, 21)]
+print(twenties)
+
+
+# Make a list of the numbers from one to one million
+one_million = [value for value in range(1, 1000001)]
+print(one_million)
+
+# What are the min, max, and sum of the list?
+print(min(one_million))
+print(max(one_million))
+print(sum(one_million))
+
+# Using the third argument of the range() function to make a list of the odd numbers from 1 to 20
+odd_numbers = list(range(1, 21, 2))
+print(odd_numbers)
+
+# Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list
+multiples_of_three = [value for value in range(3, 31, 3)]
+print(multiples_of_three) 
+
+
+cubed = [value ** 3 for value in range(1, 11)]
+print(cubed)
